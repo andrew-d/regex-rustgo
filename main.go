31e26adee0addba07ae7531e8e40b14632fea33b
Andrew Dunham <andrew@du.nham.ca>
2017-11-30 23:41:09 -0800

    This doesn't need to be a const

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
 )
 
 func main() {
-
 	fmt.Println("started")
 	defer fmt.Println("finished")
 
 	re := regex.Compile(`^\d{4}-\d{2}-\d{2}$`)
 	defer re.Free()
 
-	const test = "2017-11-12"
+	test := "2017-11-12"
 	fmt.Printf("Trying to match %q: %v\n", test, re.Match(test))
 }
